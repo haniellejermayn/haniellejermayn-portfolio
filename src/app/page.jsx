6545da697454b43d5c5c027b6a4c401ff1e62149
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-05-11 14:01:43 +0800

    globals.css color theme configuration

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="py-24">
+      <h1 className="h1">
+        Lorem, ipsum.
+      </h1>
+    </div>
+  )
+};
+
+export default page;
