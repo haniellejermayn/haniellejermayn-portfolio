a02abbdc353701432dca7997d5fa617a43daf572
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-05-11 18:30:43 +0800

    main nav layout

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import NavLinks from './NavLinks';
 
 const MainNav = () => {
   return (
-    <div>MainNav</div>
+    <nav>
+      <NavLinks containerStyles="flex flex-col gap-6" />
+    </nav>
   )
 }
 ;
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,8 +1,40 @@
+import Link from 'next/link';
 import React from 'react';
 
-const NavLinks = () => {
+const links = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "about",
+    path: "/about"
+  },
+  {
+    name: "services",
+    path: "/services"
+  },
+  {
+    name: "work",
+    path: "/work"
+  },
+  {
+    name: "contact",
+    path: "/contact"
+  }
+];
+
+const NavLinks = ({ containerStyles }) => {
   return (
-    <div>NavLinks</div>
+    <ul className={containerStyles}>
+      {links.map((link, index) => {
+        return (
+          <Link href={link.path} key={index}>
+            {link.name}
+          </Link>
+        )
+      })}
+    </ul>
   )
 };
 
