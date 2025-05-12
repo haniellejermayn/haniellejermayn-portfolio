8027eb7a4201f2f90527983e750f525d26a2c4ad
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-05-12 09:16:45 +0800

    added blob gradient AND added react-type-animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,10 @@ const Home = () => {
       className="h-screen flex items-center"
     >
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
+        {/* Text */}
         <div className="w-full xl:w-[550px]">text</div>
+
+        {/* Blob & Image */}
         <div className="hidden xl:block flex-1 relative z-20">
           {/* Blob */}
           <Blob containerStyles="w-[560px] h-[560px]" />
@@ -28,6 +31,20 @@ const Home = () => {
             height={550}
             quality={100}
             className="absolute top-[-30px] left-[30px] rounded-full"
+          />
+          {/* Overlay Gradient */}
+          {/* Smooth Overlay Gradient */}
+          <div
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: "2.5rem",
+              width: "100%",
+              height: "200px",
+              background:
+                "linear-gradient(to top, #0d120e 5%, rgba(13, 18, 14, 0.95) 15%, rgba(13, 18, 14, 0.85) 30%, rgba(13, 18, 14, 0.75) 45%, rgba(13, 18, 14, 0.6) 60%, rgba(13, 18, 14, 0.4) 75%, rgba(13, 18, 14, 0.2) 85%, rgba(13, 18, 14, 0.05) 95%, transparent 100%)",
+              borderRadius: "50%"
+            }}
           />
         </div>
       </div>
