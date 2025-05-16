da5b7f1d9a277afa7f6d16c3cd6be2ff79086c16
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-05-16 17:19:36 +0800

    updated main nav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import { MdFileDownload } from "react-icons/md";
 
 const MainNav = () => {
   return (
-    <nav className="main-nav">
-      <NavLinks containerStyles="flex flex-col gap-6 w-[100px] mx-auto" />
+    <nav className="w-full pt-16">
+      <div className="flex flex-col h-full items-center justify-between">
+        <Logo />
+        <NavLinks containerStyles="flex flex-col gap-6 w-[100px] mx-auto" />
+        <button className="btn btn-lg btn-tertiary mb-16">
+          <div className="flex items-center gap-3">
+            <span>Download CV</span>
+            <MdFileDownload className="text-xl"></MdFileDownload>
+          </div>
+        </button>
+      </div>
     </nav>
   );
 };
